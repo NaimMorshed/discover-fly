875bf97b1a7aea6dde79f2eb476ccc65b206533e
Naim Morshed <morshed.naim13@gmail.com>
2021-01-30 15:15:56 +0600

    second commit

--- script.js
+++ script.js
@@ -2,31 +2,77 @@ const firstClassPlus = document.getElementById('firstClassPlus');
 const firstClassMinus = document.getElementById('firstClassMinus');
 const economyClassPlus = document.getElementById('economyClassPlus');
 const economyClassMinus = document.getElementById('economyClassMinus');
-const firstClassInput = document.getElementById('firstClassInput');
+let firstClassInput = document.getElementById('firstClassInput');
 const economyClassInput = document.getElementById('economyClassInput');
-const subtotal = document.getElementById('subtotal');
-const vat = document.getElementById('vat');
-const total = document.getElementById('total');
+let subtotal = document.getElementById('subtotal');
+let vat = document.getElementById('vat');
+let total = document.getElementById('total');
 
-function updateSpinner(type, spin) {
-    let currentFirstClassInput = parseFloat(firstClassInput.value);
-    if (currentFirstClassInput == '') {
-        console.log('Entered');
-        currentFirstClassInput = 0;
-    } else { console.log('Not'); }
-    console.log('Value: ', currentFirstClassInput);
+function updateSpinner(classType, spin) {
+    const currentFirstClassInput = parseFloat(firstClassInput.value);
+    const currentEconomyClassInput = parseFloat(economyClassInput.value);
+    const currentSubtotal = parseFloat(subtotal.innerText);
 
-    if (type == 'firstClass') {
+    if (classType == 'firstClass') {
         if (spin == 'positiveSpin') {
-
+            firstClassInput.value = currentFirstClassInput + 1;
+            subtotal.innerText = subtotalCalculator();
+            vat.innerText = vatCalculator();
+            total.innerText = totalCalculator();
         } else if (spin == 'negativeSpin') {
-
+            if (currentFirstClassInput > 0) {
+                firstClassInput.value = currentFirstClassInput - 1;
+                subtotal.innerText = subtotalCalculator();
+                vat.innerText = vatCalculator();
+                total.innerText = totalCalculator();
+            }
         }
-    } else if (type == 'economyClass') {
-
+    } else if (classType == 'economyClass') {
+        if (spin == 'positiveSpin') {
+            economyClassInput.value = currentEconomyClassInput + 1;
+            subtotal.innerText = subtotalCalculator();
+            vat.innerText = vatCalculator();
+            total.innerText = totalCalculator();
+        } else if (spin == 'negativeSpin') {
+            if (currentEconomyClassInput > 0) {
+                economyClassInput.value = currentEconomyClassInput - 1;
+                subtotal.innerText = subtotalCalculator();
+                vat.innerText = vatCalculator();
+                total.innerText = totalCalculator();
+            }
+        }
     }
 }
+function subtotalCalculator() {
+    const currentFirstClassInput = parseFloat(firstClassInput.value);
+    const currentEconomyClassInput = parseFloat(economyClassInput.value);
+    return (currentFirstClassInput * 150) + (currentEconomyClassInput * 100);
+}
+function vatCalculator() {
+    const currentSubtotal = parseFloat(subtotal.innerText);
+    return (currentSubtotal * 10) / 100;
+}
+function totalCalculator() {
+    const currentSubtotal = parseFloat(subtotal.innerText);
+    const currentVat = parseFloat(vat.innerText);
+    return currentSubtotal + currentVat;
+}
 
-firstClassPlus.addEventListener('click', function(){
+firstClassPlus.addEventListener('click', function () {
     updateSpinner('firstClass', 'positiveSpin')
 });
+firstClassMinus.addEventListener('click', function () {
+    updateSpinner('firstClass', 'negativeSpin')
+});
+economyClassPlus.addEventListener('click', function () {
+    updateSpinner('economyClass', 'positiveSpin')
+});
+economyClassMinus.addEventListener('click', function () {
+    updateSpinner('economyClass', 'negativeSpin')
+});
+
+function bookNowButton() {
+    document.getElementById('mainPage').style.display = 'none';
+    document.getElementById('confirmationPage').style.display = 'block';
+}
+
